a022bdd295764c48776423cdb1065102429c87fb
nazmus <nazmus@webalive.com.au>
2024-05-12 10:04:55 +0600

    tailwind initialized

--- starter-frontend/src/App.jsx
+++ starter-frontend/src/App.jsx
@@ -5,9 +5,9 @@ function App() {
   return (
     <>
       <div>
-          <p>
-              I am here...
-          </p>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
       </div>
 
     </>

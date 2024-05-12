97edef706283d99ff491d302d57e14fc4f60760a
nazmus <nazmus@webalive.com.au>
2024-05-12 11:56:01 +0600

    router gurd implemented

--- starter-frontend/src/routes/Routes.jsx
+++ starter-frontend/src/routes/Routes.jsx
@@ -1,11 +1,25 @@
-import React, { lazy, Suspense } from "react";
+import React, { useState, useEffect, lazy, Suspense } from "react";
 import PropTypes from "prop-types";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 const Login = lazy(() => import("../pages/Login.jsx"));
 const Registration = lazy(() => import("../pages/Registration.jsx"));
+const Dashboard = lazy(() => import("../pages/Dashboard.jsx"));
 
 const Routers = () => {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setAuthenticated(true);
+    }
+  }, []);
+
+  const PrivateRoute = ({ element, ...rest }) => {
+    return authenticated ? element : <Navigate to="/" />;
+  };
+
   return (
     <>
       <Suspense>
@@ -18,6 +32,7 @@ const Routers = () => {
             path="/signup"
             element={<Registration />}
           />
+          <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
         </Routes>
       </Suspense>
     </>
